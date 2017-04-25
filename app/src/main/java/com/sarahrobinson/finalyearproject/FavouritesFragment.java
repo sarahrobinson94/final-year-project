63e4b890182582b48d9a2f619870068130de0065
Sarah Robinson <sarah.robinson@sixdegreelabs.com>
2017-04-25 18:17:19 +0100

    again (removing unused imports)

--- app/src/main/java/com/sarahrobinson/finalyearproject/FavouritesFragment.java
+++ app/src/main/java/com/sarahrobinson/finalyearproject/FavouritesFragment.java
@@ -2,7 +2,6 @@ package com.sarahrobinson.finalyearproject;
 
 import android.content.Context;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
@@ -10,13 +9,10 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.ListView;
 import android.widget.TextView;
 
-import com.firebase.client.Firebase;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -24,16 +20,11 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-import com.squareup.picasso.Picasso;
 
+import com.squareup.picasso.Picasso;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 import static com.sarahrobinson.finalyearproject.MainActivity.currentUserId;
-import static com.sarahrobinson.finalyearproject.MainActivity.firebaseRef;
-import static com.sarahrobinson.finalyearproject.MapFragment.selectedPlaceId;
 
 public class FavouritesFragment extends Fragment implements View.OnClickListener{
 

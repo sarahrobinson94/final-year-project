b5bb93bd504e462279c69a71b6b09781febaff7b
Sarah Robinson <sarah.robinson@sixdegreelabs.com>
2017-05-01 04:58:07 +0100

    adding fragments for find friends tabs

--- app/src/main/java/com/sarahrobinson/finalyearproject/fragments/FindFriendsFragment.java
+++ app/src/main/java/com/sarahrobinson/finalyearproject/fragments/FindFriendsFragment.java
@@ -1,30 +1,71 @@
 package com.sarahrobinson.finalyearproject.fragments;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTabHost;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.sarahrobinson.finalyearproject.R;
+import com.sarahrobinson.finalyearproject.activities.LoginActivity;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class FindFriendsFragment extends Fragment {
 
+    private FirebaseAuth firebaseAuth;
+    private FirebaseUser firebaseUser;
+
+    private FragmentTabHost tabHost;
 
     public FindFriendsFragment() {
         // Required empty public constructor
     }
 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseUser = firebaseAuth.getCurrentUser();
+
+        // starting login activity if user is not logged in
+        if(firebaseAuth.getCurrentUser() == null){
+            startActivity(new Intent(getActivity(), LoginActivity.class));
+            getActivity().finish();
+        }else {
+            // stay in fragment
+        }
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_find_friends, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_find_friends, container, false);
+
+        // changing actionBar title
+        getActivity().setTitle("Find Friends");
+
+        // setting up tabs
+        tabHost = (FragmentTabHost)rootView.findViewById(android.R.id.tabhost);
+        tabHost.setup(getActivity(), getChildFragmentManager(), R.layout.fragment_find_friends);
+
+        tabHost.addTab(tabHost.newTabSpec("tab1").setIndicator("FACEBOOK"),
+                FindFriendsFragmentTabFacebook.class, null);
+        tabHost.addTab(tabHost.newTabSpec("tab2").setIndicator("GOOGLE"),
+                FindFriendsFragmentTabGoogle.class, null);
+        tabHost.addTab(tabHost.newTabSpec("tab3").setIndicator("EMAIL"),
+                FindFriendsFragmentTabEmail.class, null);
+
+        // TODO: 19/03/2017 get name from database and add ValueEventListener ?? (see android bash blog post)
+        return rootView;
     }
 
 }
---
+++ app/src/main/java/com/sarahrobinson/finalyearproject/fragments/FindFriendsFragmentTabEmail.java
@@ -0,0 +1,94 @@
+package com.sarahrobinson.finalyearproject.fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.fasterxml.jackson.databind.ser.std.IterableSerializer;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
+import com.sarahrobinson.finalyearproject.R;
+import com.sarahrobinson.finalyearproject.classes.Event;
+import com.sarahrobinson.finalyearproject.classes.User;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.sarahrobinson.finalyearproject.activities.MainActivity.databaseRef;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class FindFriendsFragmentTabEmail extends Fragment implements View.OnClickListener {
+
+    private static final String TAG = "TabEmail ******* ";
+
+    private EditText txtEmailSearch;
+    private LinearLayout layoutEmailUser;
+    private ImageView imgEmailUserImg;
+    private TextView tvEmailUserId;
+    private TextView tvEmailUserName;
+    private Button btnFindEmailUser;
+
+    private String searchedEmail;
+
+    public FindFriendsFragmentTabEmail() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootView = inflater.inflate(R.layout.fragment_find_friends_tab_email, container, false);
+
+        // getting views
+        txtEmailSearch = (EditText)rootView.findViewById(R.id.editTextFindEmailUser);
+        layoutEmailUser = (LinearLayout) rootView.findViewById(R.id.tabLayoutEmailUser);
+        imgEmailUserImg = (ImageView)rootView.findViewById(R.id.tabEmailUserImg);
+        tvEmailUserId = (TextView) rootView.findViewById(R.id.tabEmailUserId);
+        tvEmailUserName = (TextView) rootView.findViewById(R.id.tabEmailUserName);
+        btnFindEmailUser = (Button) rootView.findViewById(R.id.btnFindEmailUser);
+
+        // hiding search result layout until user is found
+        layoutEmailUser.setVisibility(View.GONE);
+
+        // find user button onclick listener
+        btnFindEmailUser.setOnClickListener(this);
+
+        return rootView;
+    }
+
+    private void findEmailUser() {
+        // getting searched email
+        searchedEmail = txtEmailSearch.getText().toString();
+
+        
+    }
+
+    private void showEmailUser() {
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == btnFindEmailUser) {
+            Log.d(TAG, "btnFindEmailUser clicked");
+            findEmailUser();
+        }
+    }
+}

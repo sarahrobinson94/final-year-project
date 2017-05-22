ce7656dd4d10324c960f373c852432a0193c1a80
Sarah Robinson <sarah.robinson@sixdegreelabs.com>
2017-05-22 21:44:09 +0100

    adding styling to tabs on find friends screen

--- app/src/main/java/com/sarahrobinson/finalyearproject/fragments/FindFriendsFragment.java
+++ app/src/main/java/com/sarahrobinson/finalyearproject/fragments/FindFriendsFragment.java
@@ -2,17 +2,22 @@ package com.sarahrobinson.finalyearproject.fragments;
 
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTabHost;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TabHost;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.sarahrobinson.finalyearproject.R;
 import com.sarahrobinson.finalyearproject.activities.LoginActivity;
+
+import static android.R.id.tabhost;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -22,7 +27,7 @@ public class FindFriendsFragment extends Fragment {
     private FirebaseAuth firebaseAuth;
     private FirebaseUser firebaseUser;
 
-    private FragmentTabHost tabHost;
+    private FragmentTabHost mTabHost;
 
     public FindFriendsFragment() {
         // Required empty public constructor
@@ -54,18 +59,50 @@ public class FindFriendsFragment extends Fragment {
         getActivity().setTitle("Find Friends");
 
         // setting up tabs
-        tabHost = (FragmentTabHost)rootView.findViewById(android.R.id.tabhost);
-        tabHost.setup(getActivity(), getChildFragmentManager(), R.layout.fragment_find_friends);
+        mTabHost = (FragmentTabHost)rootView.findViewById(tabhost);
+        mTabHost.setup(getActivity(), getChildFragmentManager(), android.R.id.tabcontent);
 
-        tabHost.addTab(tabHost.newTabSpec("tab1").setIndicator("FACEBOOK"),
+        mTabHost.addTab(
+                mTabHost.newTabSpec("tab1").setIndicator("FACEBOOK", null),
                 FindFriendsFragmentTabFacebook.class, null);
-        tabHost.addTab(tabHost.newTabSpec("tab2").setIndicator("GOOGLE"),
+        mTabHost.addTab(
+                mTabHost.newTabSpec("tab2").setIndicator("GOOGLE", null),
                 FindFriendsFragmentTabGoogle.class, null);
-        tabHost.addTab(tabHost.newTabSpec("tab3").setIndicator("EMAIL"),
+        mTabHost.addTab(
+                mTabHost.newTabSpec("tab3").setIndicator("EMAIL", null),
                 FindFriendsFragmentTabEmail.class, null);
+
+        // setting tab text colours
+        mTabHost.setOnTabChangedListener(new TabHost.OnTabChangeListener() {
+            @Override
+            public void onTabChanged(String tabId) {
+                // unselected tabs
+                for (int i = 0; i < mTabHost.getTabWidget().getChildCount(); i++) {
+                    TextView tv = (TextView) mTabHost.getTabWidget().getChildAt(i).findViewById(android.R.id.title);
+                    tv.setTextColor(Color.parseColor("#ADADAD"));
+                }
+                // selected tab
+                TextView tv = (TextView) mTabHost.getCurrentTabView().findViewById(android.R.id.title);
+                tv.setTextColor(Color.parseColor("#666666"));
+            }
+        });
 
         // TODO: 19/03/2017 get name from database and add ValueEventListener ?? (see android bash blog post)
         return rootView;
     }
 
+    @Override
+    public void onResume() {
+        // setting inital tab text colour
+        for(int i=0;i<mTabHost.getTabWidget().getChildCount();i++)
+        {
+            // unselected tabs
+            TextView tvUnselected = (TextView)mTabHost.getTabWidget().getChildAt(i).findViewById(android.R.id.title);
+            tvUnselected.setTextColor(Color.parseColor("#ADADAD"));
+            // selected tab
+            TextView tvSelected = (TextView) mTabHost.getCurrentTabView().findViewById(android.R.id.title); // selected tab
+            tvSelected.setTextColor(Color.parseColor("#666666"));
+        }
+        super.onResume();
+    }
 }
